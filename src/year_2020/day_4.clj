9ab0a2d8f795ce275d7f761debc954c4b897195b
myhan <hmy3743@gmail.com>
2022-01-24 16:08:49 +0900

    Apply code review year 2020 day 4

--- src/year_2020/day_4.clj
+++ src/year_2020/day_4.clj
@@ -17,7 +17,9 @@
   (->> passport-string
        (re-seq #"([^:\s]+):([^:\s]+)")
        (map #(subvec % 1))
-       (into {})))
+       (into {})
+       (#(select-keys % ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid"]))
+       (#(zipmap (map keyword (keys %)) (vals %)))))
 
 (defn parse-input-into-passport
   [input]
@@ -32,7 +34,7 @@
 (defn solve-part-1
   [passports]
   (->> passports
-       (filter #(passport-field-valid? ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"] %))
+       (filter #(passport-field-valid? [:byr :iyr :eyr :hgt :hcl :ecl :pid] %))
        count))
 
 
@@ -58,19 +60,19 @@
     false))
 
 (defn valid-byr?
-  [{byr "byr"}]
+  [{byr :byr}]
   (int-in? 1920 2003 byr))
 
 (defn valid-iyr?
-  [{iyr "iyr"}]
+  [{iyr :iyr}]
   (int-in? 2010 2021 iyr))
 
 (defn valid-eyr?
-  [{eyr "eyr"}]
+  [{eyr :eyr}]
   (int-in? 2020 2031 eyr))
 
 (defn valid-hgt?
-  [{hgt "hgt"}]
+  [{hgt :hgt}]
   (if hgt (let [bottoms {"in" 59 "cm" 150}
                 tops {"in" 77 "cm" 194}
                 [_ val unit] (re-matches #"(\d+)(cm|in)" hgt)]
@@ -80,15 +82,15 @@
           false))
 
 (defn valid-hcl?
-  [{hcl "hcl"}]
+  [{hcl :hcl}]
   (some? (when hcl (re-matches #"#[0-9a-f]{6}" hcl))))
 
 (defn valid-ecl?
-  [{ecl "ecl"}]
+  [{ecl :ecl}]
   (contains? #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} ecl))
 
 (defn valid-pid?
-  [{pid "pid"}]
+  [{pid :pid}]
   (some? (when pid (re-matches #"\d{9}" pid))))
 
 (defn solve-part-2
@@ -138,34 +140,34 @@
     "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\n"
     )
   (passport-field-valid?
-    ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"]
-    {"ecl" "gry", "pid" "860033327", "eyr" "2020", "hcl" "#fffffd", "byr" "1937", "iyr" "2017", "cid" "147", "hgt" "183cm"}
+    [:byr :iyr :eyr :hgt :hcl :ecl :pid]
+    {:ecl "gry", :pid "860033327", :eyr "2020", :hcl "#fffffd", :byr "1937", :iyr "2017", :cid "147", :hgt "183cm"}
     )
   (passport-field-valid?
-    ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"]
-    {"iyr" "2013", "ecl" "amb", "cid" "350", "eyr" "2023", "pid" "028048884", "hcl" "#cfa07d", "byr" "1929"}
+    [:byr :iyr :eyr :hgt :hcl :ecl :pid]
+    {:iyr "2013", :ecl "amb", :cid "350", :eyr "2023", :pid "028048884", :hcl "#cfa07d", :byr "1929"}
     )
-  (solve-part-1 '({"ecl" "gry", "pid" "860033327", "eyr" "2020", "hcl" "#fffffd", "byr" "1937", "iyr" "2017", "cid" "147", "hgt" "183cm"}
-                  {"iyr" "2013", "ecl" "amb", "cid" "350", "eyr" "2023", "pid" "028048884", "hcl" "#cfa07d", "byr" "1929"})
+  (solve-part-1 '({:ecl "gry", :pid "860033327", :eyr "2020", :hcl "#fffffd", :byr "1937", :iyr "2017", :cid "147", :hgt "183cm"}
+                  {:iyr "2013", :ecl "amb", :cid "350", :eyr "2023", :pid "028048884", :hcl "#cfa07d", :byr "1929"})
                 )
   (main-part-1 "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\n")
   (-> "resources/year_2020/day_4.in"
       slurp
       main-part-1)
-  (valid-byr? {"ecl" "gry", "pid" "860033327", "eyr" "2020", "hcl" "#fffffd", "byr" "1937", "iyr" "2017", "cid" "147", "hgt" "183cm"}
+  (valid-byr? {:ecl "gry", :pid "860033327", :eyr "2020", :hcl "#fffffd", :byr "1937", :iyr "2017", :cid "147", :hgt "183cm"}
               )
-  (valid-byr? {"byr" "1992"})
-  (valid-byr? {"byr" "1919"})
-  (valid-byr? {"byr" "2003"})
+  (valid-byr? {:byr "1992"})
+  (valid-byr? {:byr "1919"})
+  (valid-byr? {:byr "2003"})
   (valid-byr? {})
   (valid-hgt? {})
-  (valid-hgt? {"hgt" "1.8m"})
-  (map valid-hgt? '({"hgt" "58in"} {"hgt" "59in"} {"hgt" "76in"} {"hgt" "77in"} {"hgt" "149cm"} {"hgt" "150cm"} {"hgt" "193cm"} {"hgt" "194cm"}))
+  (valid-hgt? {:hgt "1.8m"})
+  (map valid-hgt? '({:hgt "58in"} {:hgt "59in"} {:hgt "76in"} {:hgt "77in"} {:hgt "149cm"} {:hgt "150cm"} {:hgt "193cm"} {:hgt "194cm"}))
   (valid-hcl? {})
-  (map valid-hcl? '({"hcl" "#123123"} {"hcl" "123123"} {"hcl" "#FFFFFF"}))
-  (map valid-ecl? '({"ecl" "amb"} {"ecl" "blu"} {"ecl" "brn"} {"ecl" "gry"} {"ecl" "grn"} {"ecl" "hzl"} {"ecl" "oth"} {"ecl" "png"}))
+  (map valid-hcl? '({:hcl "#123123"} {:hcl "123123"} {:hcl "#FFFFFF"}))
+  (map valid-ecl? '({:ecl "amb"} {:ecl "blu"} {:ecl "brn"} {:ecl "gry"} {:ecl "grn"} {:ecl "hzl"} {:ecl "oth"} {:ecl "png"}))
   (valid-pid? {})
-  (map valid-pid? '({"pid" "123123123"} {"pid" "023123123"} {"pid" "23123123"}))
+  (map valid-pid? '({:pid "123123123"} {:pid "023123123"} {:pid "23123123"}))
   (-> "resources/year_2020/day_4.in"
       slurp
       main-part-2)
--- test/year_2020/day_4.clj
+++ test/year_2020/day_4.clj
@@ -7,32 +7,32 @@
                      "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929")
                   (parse-input-into-chunk
                     "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\n"))))
-  (testing (is (= {"ecl" "gry", "pid" "860033327", "eyr" "2020", "hcl" "#fffffd", "byr" "1937", "iyr" "2017", "cid" "147", "hgt" "183cm"}
+  (testing (is (= {:ecl "gry", :pid "860033327", :eyr "2020", :hcl "#fffffd", :byr "1937", :iyr "2017", :cid "147", :hgt "183cm"}
                   (parse-passport "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm"))))
-  (testing (is (= '({"ecl" "gry", "pid" "860033327", "eyr" "2020", "hcl" "#fffffd", "byr" "1937", "iyr" "2017", "cid" "147", "hgt" "183cm"}
-                    {"iyr" "2013", "ecl" "amb", "cid" "350", "eyr" "2023", "pid" "028048884", "hcl" "#cfa07d", "byr" "1929"})
+  (testing (is (= '({:ecl "gry", :pid "860033327", :eyr "2020", :hcl "#fffffd", :byr "1937", :iyr "2017", :cid "147", :hgt "183cm"}
+                    {:iyr "2013", :ecl "amb", :cid "350", :eyr "2023", :pid "028048884", :hcl "#cfa07d", :byr "1929"})
                   (parse-input-into-passport
                     "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\n"))))
   (testing (is (= true
                   (passport-field-valid?
-                    ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"]
-                    {"ecl" "gry", "pid" "860033327", "eyr" "2020", "hcl" "#fffffd", "byr" "1937", "iyr" "2017", "cid" "147", "hgt" "183cm"}
+                    [:byr :iyr :eyr :hgt :hcl :ecl :pid]
+                    {:ecl "gry", :pid "860033327", :eyr "2020", :hcl "#fffffd", :byr "1937", :iyr "2017", :cid "147", :hgt "183cm"}
                     ))))
   (testing (is (= false
                   (passport-field-valid?
                     ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"]
-                    {"iyr" "2013", "ecl" "amb", "cid" "350", "eyr" "2023", "pid" "028048884", "hcl" "#cfa07d", "byr" "1929"}
+                    {:iyr "2013", :ecl "amb", :cid "350", :eyr "2023", :pid "028048884", :hcl "#cfa07d", :byr "1929"}
                     ))))
   (testing (is (= 1 (main-part-1 "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\n"))))
   (testing (is (= 190 (-> "resources/year_2020/day_4.in"
                           slurp
                           main-part-1))))
-  (testing (is (= true (valid-byr? {"ecl" "gry", "pid" "860033327", "eyr" "2020", "hcl" "#fffffd", "byr" "1937", "iyr" "2017", "cid" "147", "hgt" "183cm"}))))
-  (testing (is (= '(true false false false) (map valid-byr? '({"byr" "1992"} {"byr" "1919"} {"byr" "2003"} {})))))
-  (testing (is (= '(false false false true true false false true true false) (map valid-hgt? '({} {"hgt" "1.8m"} {"hgt" "58in"} {"hgt" "59in"} {"hgt" "76in"} {"hgt" "77in"} {"hgt" "149cm"} {"hgt" "150cm"} {"hgt" "193cm"} {"hgt" "194cm"})))))
-  (testing (is (= '(false true false false) (map valid-hcl? '({} {"hcl" "#123123"} {"hcl" "123123"} {"hcl" "#FFFFFF"})))))
-  (testing (is (= '(false true true true true true true true false) (map valid-ecl? '({} {"ecl" "amb"} {"ecl" "blu"} {"ecl" "brn"} {"ecl" "gry"} {"ecl" "grn"} {"ecl" "hzl"} {"ecl" "oth"} {"ecl" "png"})))))
-  (testing (is (= '(false true true false) (map valid-pid? '({} {"pid" "123123123"} {"pid" "023123123"} {"pid" "23123123"})))))
+  (testing (is (= true (valid-byr? {:ecl "gry", :pid "860033327", :eyr "2020", :hcl "#fffffd", :byr "1937", :iyr "2017", :cid "147", :hgt "183cm"}))))
+  (testing (is (= '(true false false false) (map valid-byr? '({:byr "1992"} {:byr "1919"} {:byr "2003"} {})))))
+  (testing (is (= '(false false false true true false false true true false) (map valid-hgt? '({} {:hgt "1.8m"} {:hgt "58in"} {:hgt "59in"} {:hgt "76in"} {:hgt "77in"} {:hgt "149cm"} {:hgt "150cm"} {:hgt "193cm"} {:hgt "194cm"})))))
+  (testing (is (= '(false true false false) (map valid-hcl? '({} {:hcl "#123123"} {:hcl "123123"} {:hcl "#FFFFFF"})))))
+  (testing (is (= '(false true true true true true true true false) (map valid-ecl? '({} {:ecl "amb"} {:ecl "blu"} {:ecl "brn"} {:ecl "gry"} {:ecl "grn"} {:ecl "hzl"} {:ecl "oth"} {:ecl "png"})))))
+  (testing (is (= '(false true true false) (map valid-pid? '({} {:pid "123123123"} {:pid "023123123"} {:pid "23123123"})))))
   (testing (is (= 121 (-> "resources/year_2020/day_4.in"
                           slurp
                           main-part-2))))
